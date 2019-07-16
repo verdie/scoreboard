4cd50bdadf739fdcf87b7cd4dfe617cfb4f4986b
halynaburdiian <verdamtion@gmail.com>
2019-07-16 13:52:47 +0200

    Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,53 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export default class Player extends Component{
+export default class Player extends Component {
     static propTypes = {
-        content: PropTypes.string.isRequired,
+          id: PropTypes.number.isRequired,
+          name: PropTypes.string.isRequired,
+          score: PropTypes.number.isRequired,
+          incrementScore: PropTypes.func.isRequired
     }
-    constructor(){
-        super()
-        // this.props.name = '';
-        // this.props.score = '';
-        this.state =[name : '', score : '']
+      
+    handleClick = () => {
+          this.incrementScore(this.props.id);
     }
-    
-    render(){
+      
+    render() {
         return (
             <li className="player">
-              <p className="name">{this.state}</p>
+                <p className="name">{ this.props.name }</p>
+                <p className="score">{ this.props.score }</p>
+                <button className="plus-button" onClick={this.handleClick}>+</button>
             </li>
         )
     }
+    renderPlayer = player => {
+        return (
+          <Player
+            id={player.id}
+            name={player.name}
+            score={player.score}
+            key={player.id}
+            incrementScore={this.incrementScoreOfPlayer}
+          />
+        )
+      }
+    
+      incrementScoreOfPlayer = id => {
+       // Making a new array with the same objects except for the
+    //  one that should be updated,
+    const updatedPlayers = this.state.players.map(player => {
+        if (player.id === id) {
+          // ...which we replace with a copy of the original,
+          //  except for the property `score` which is incremented
+          return { ...player, score: player.score + 1 };
+        } else {
+          return player;
+        }
+      });
+      // Finally, we use `this.setState` to replace the players array
+      this.setState({ players: updatedPlayers });
+      }
     
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -6,17 +6,58 @@ export default class Scoreboard extends Component {
     static propTypes = {
         content: PropTypes.string.isRequired,
     }
-    render (){
-        return (
-        <div className="scoreboard">
-        <h1>Scoreboard</h1>
-        <ul>
-          <Player name="Arien" score = '5' content = ''/>
-          <Player name="David" score = '3'content = '' />
-          <Player name="Mimi" score = '2'content = ''/>
-        </ul>
-      </div>
-      )
+
+    constructor() {
+      super();
+
+      this.state = {
+        players: [
+          {id : 1, name: 'Arien', score: '2'},
+          {id : 2, name: 'David', score: '5'},
+          {id : 3, name: 'Mimi', score: '4'},
+        ]
+      };
     }
+
+    // render (){
+    //     return (
+    //     <div className="scoreboard">
+    //     <h1>Scoreboard</h1>
+    //     <ul>
+    //       {this.state.players.map((player) => {
+    //         return <Player name={player.name} score={player.score} key = {player.id}content = '' />
+    //       })}
+    //     </ul>
+    //   </div>
+    //   )
+    // }
+    render () {
+        // copying the array of players because `.sort()` **mutates!**
+        const players_copy = [ ...this.state.players ];
+        // sorting the players
+        players_copy.sort((a, b) => b.score - a.score);
+        // console.log(players_copy); // <!-- add console.log's if you're not sure!
     
+        return (
+          <div className="scoreboard">
+            <h1>Scoreboard</h1>
+            <ul>
+              {players_copy.map(this.renderPlayer)}
+            </ul>
+          </div>
+        );
+      }
+    
+      renderPlayer = player => {
+        return (
+          <Player
+            id={player.id}
+            name={player.name}
+            score={player.score}
+            key={player.id}
+            incrementScore={this.incrementScoreOfPlayer}
+            content =''
+          />
+        );
+    }
 }
